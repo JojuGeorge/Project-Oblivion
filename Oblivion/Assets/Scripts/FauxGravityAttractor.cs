dc113008fe4d3fb19afcdf6ab49259ae295043d8
JojuGeorge <jojugeorge001@gmail.com>
2019-03-14 07:45:43 -0700

    initial commit

---
+++ Oblivion/Assets/Scripts/FauxGravityAttractor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityAttractor : MonoBehaviour {
+
+    //  THIS SCRIPT WORKS AS THE PLANETS GRAVITATION PULL. PULLING ALL THE FauxGravityBody.cs script attached objects to it
+
+    public float gravity = -10f;
+
+    public void Attract(Transform body) {
+        Vector3 gravityUp = (body.position - transform.position).normalized;    //dir of the player/obj from the center of the planet
+        Vector3 bodyUp = body.up;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+}
